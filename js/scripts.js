b4f25f9f76656f314c28f1973e43033f0b5a4a3c
Max Scher <maxobaxo@gmail.com>
2017-06-16 08:59:01 -0700

    add newOrder object constructor to store order selection details

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+//business logic
+function Order(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+};
+
+//user interface logic
 $(document).ready(function() {
-  
+  $("#order-form").submit(function(event) {
+    event.preventDefault();
+
+    var sizeInput = $("#size").val();
+    var toppingsInput = [];
+    $("input:checkbox[name=toppings]:checked").each(function() {
+          var toppingSelection = $(this).val();
+          toppingsInput.push(toppingSelection);
+        });
+
+    var newOrder = new Order(sizeInput, toppingsInput);
+
+    // $("#order-confirm").show();
+    // var sizeInput = $("#size").val();
+    // $(".size").text(sizeInput);
+  });
 });
