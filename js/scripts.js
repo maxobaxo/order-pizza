69a7ae3616464610606685fa195a56d7ce9ed2f4
Max Scher <maxobaxo@gmail.com>
2017-06-16 09:27:32 -0700

    add price prototype with logic based on user selections

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,37 @@ function Order(size, toppings) {
   this.toppings = toppings;
 };
 
+Order.prototype.price = function() {
+  var pizzaTotal = 0;
+
+  if (this.size === "small") {
+    pizzaTotal += 8;
+  } else if (this.size === "medium") {
+    pizzaTotal += 10;
+  } else {
+    pizzaTotal += 12;
+  }
+
+  this.toppings.forEach(function(item) {
+    if (item === "cheese") {
+      pizzaTotal += 1;
+    }
+    if (item === "pepperoni") {
+      pizzaTotal += 2;
+    }
+    if (item === "olives") {
+      pizzaTotal += 1;
+    }
+    if (item === "sausage") {
+      pizzaTotal += 3;
+    }
+    if (item === "anchovy") {
+      pizzaTotal += 2;
+    }
+  });
+  return pizzaTotal;
+};
+
 //user interface logic
 $(document).ready(function() {
   $("#order-form").submit(function(event) {
@@ -12,14 +43,18 @@ $(document).ready(function() {
     var sizeInput = $("#size").val();
     var toppingsInput = [];
     $("input:checkbox[name=toppings]:checked").each(function() {
-          var toppingSelection = $(this).val();
-          toppingsInput.push(toppingSelection);
-        });
-
+      var toppingSelection = $(this).val();
+      toppingsInput.push(toppingSelection);
+    });
     var newOrder = new Order(sizeInput, toppingsInput);
+console.log(newOrder);
+console.log(newOrder.toppings);
+console.log(newOrder.price());
 
-    // $("#order-confirm").show();
-    // var sizeInput = $("#size").val();
-    // $(".size").text(sizeInput);
+    $(".size").text(newOrder.size)
+    newOrder.toppings.forEach(function(item) {
+      $(".toppings").append("<li>" + item + "</li>");
+    });
+    $("#order-confirm").show();
   });
 });
